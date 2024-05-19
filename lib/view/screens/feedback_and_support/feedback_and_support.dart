cc0b58084eb114a1cc4e3f05f67adc7d768cc341
muhammed-shadil <muhammedshadil220@gmail.com>
2024-05-20 01:10:29 +0530

    settings page constants

---
+++ lib/view/screens/feedback_and_support/feedback_and_support.dart
@@ -0,0 +1,91 @@
+import 'package:doctors_book_app/utility/constants.dart';
+import 'package:doctors_book_app/view/screens/aboutus/text_and_details.dart';
+import 'package:doctors_book_app/view/screens/aboutus/unorderedlist.dart';
+import 'package:flutter/material.dart';
+
+class FeedbackandSupportScreen extends StatelessWidget {
+  const FeedbackandSupportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(20),
+                    width: 37,
+                    height: 35,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 186, 183, 183)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          size: 22,
+                          color: Color.fromARGB(255, 151, 151, 150),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 23),
+                    child: Text(
+                      "Feedback and Support",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 118, 115, 115)),
+                    ),
+                  )
+                ],
+              ),
+              Container(padding: EdgeInsets.all(16),
+                child: Text(
+                    "Welcome to the Feedback and Support page of MEDICO. We value your feedback and are here to assist you with any questions or concerns you may have. Here's how you can reach out to us: ",style: TextStyle(fontSize: 18),),
+              ),
+              const Contents(
+                title: "Contact Us",
+                content:
+                    "If you have any questions, feedback, or suggestions regarding our app, please don't hesitate to contact our support team. You can reach us via email at [support email]. We strive to respond to all inquiries promptly and provide you with the assistance you need.",
+              ),
+              const Contents(
+                title: "Help Center",
+                content:
+                    "For answers to common questions or troubleshooting tips, please visit our Help Center. Here, you'll find helpful articles and resources to guide you through using our app effectively. Access the Help Center directly from the app's menu or visit our website at [Help Center URL].",
+              ),
+              const Contents(
+                title: "Report an Issue",
+                content:
+                    "Encountered a problem or bug while using our app? Please let us know by reporting the issue to our support team. You can report issues directly from the app by navigating to the 'Report an Issue' section in the settings menu. Be sure to provide detailed information about the problem you're experiencing so that we can address it promptly.",
+              ),
+              const Contents(
+                  title: "Feedback Form",
+                  content:
+                      "We value your input and are always looking for ways to improve our app. If you have any feedback or suggestions for us, please fill out our feedback form. Your feedback helps us understand your needs and preferences better, allowing us to make enhancements that benefit all users."),
+              const Contents(
+                  title: "Stay Connected",
+                  content:
+                      "Follow us on social media to stay updated on the latest news, updates, and promotions. Connect with us on [social media platforms] to join our community and engage with other users."),
+              Container(
+                padding: EdgeInsets.all(14),
+                child: const Text(
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    "Thank you for choosing MEDICO. We're committed to providing you with an exceptional experience and are here to support you every step of the way."),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/screens/privacypolicy/privacypolicy.dart
@@ -0,0 +1,127 @@
+import 'package:doctors_book_app/utility/constants.dart';
+import 'package:doctors_book_app/view/screens/aboutus/text_and_details.dart';
+import 'package:doctors_book_app/view/screens/aboutus/unorderedlist.dart';
+import 'package:flutter/material.dart';
+
+class PrivacypolicyScreen extends StatelessWidget {
+  const PrivacypolicyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(20),
+                    width: 37,
+                    height: 35,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 186, 183, 183)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          size: 22,
+                          color: Color.fromARGB(255, 151, 151, 150),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 40),
+                    child: Text(
+                      "Privacy Policy",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 118, 115, 115)),
+                    ),
+                  )
+                ],
+              ),
+              const Contents(
+                  title: "Last Updated: 20-05-2024",
+                  content:
+                      "MEDICO  is committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our doctor booking app, [App Name]. Please read this Privacy Policy carefully. By using our app, you consent to the collection, use, disclosure, and safeguarding of your information in accordance with this Privacy Policy."),
+              const Contents(
+                title: "Data Security",
+                content:
+                    "We implement reasonable measures to safeguard your information from unauthorized access, use, or disclosure. However, no method of transmission over the internet or electronic storage is 100% secure.",
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Information We Collect",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colorpalette.primarycolor,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    BulletList([
+                      'Personal Information: When you register an account, we may collect personal information such as your name, email address, phone number, and date of birth.',
+                      "Booking Information: We collect information about the appointments you book through our app, including the doctor's name, appointment date and time, and any notes or preferences you provide.",
+                      "Usage Data: We may collect information about how you interact with our app, including the features you use and the actions you take.",
+                    ])
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "How We Use Your Information",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colorpalette.primarycolor,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    BulletList([
+                      'To Provide Services: We use your information to facilitate appointment bookings, send appointment reminders, and provide customer support.',
+                      'To Improve Our App: We may use your information to analyze app usage trends, identify areas for improvement, and enhance the user experience.',
+                      "For Marketing Purposes: With your consent, we may send you promotional communications about our app and related products or services.",
+                    ])
+                  ],
+                ),
+              ),
+              const Contents(
+                title: "Your Choices",
+                content:
+                    "You can update or delete your account information at any time by accessing your account settings in the app. You may also opt out of receiving promotional communications from us by following the instructions provided in the communication.",
+              ),
+              const Contents(
+                title: "Children's Privacy",
+                content:
+                    "Our app is not intended for use by children under the age of 13. We do not knowingly collect personal information from children under 13.",
+              ),
+              const Contents(
+                  title: "Changes to This Privacy Policy",
+                  content:
+                      "We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page."),
+              const Contents(
+                  title: "Contact Us",
+                  content:
+                      "If you have any questions or concerns about this Privacy Policy, please contact us at muhammedshadil220@gmail.com .")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
